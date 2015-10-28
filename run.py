192ec734b177e2b11a770113dfa27b5a21eb14c1
martimontane <martin_montane_6@hotmail.com>
2015-10-28 15:35:17 -0300

    cambio de mapay salto arreglados

--- run.py
+++ run.py
@@ -10,12 +10,13 @@ pilas.comportamientos.vincular(Subir)
 
 class Kuro(pilasengine.actores.Actor):
     def iniciar(self):
-        self.imagen = "mario.png"
-        self.figura = pilas.fisica.Rectangulo(self.x, self.y, 17)
+        self.imagen = "data/Kuro/mario.png"
+        self.figura = pilas.fisica.Rectangulo(self.x, self.y, 20, 33)
         self.figura.sin_rotacion = True
-        self.figura.escala_de_gravedad = 2
+        self.figura.escala_de_gravedad = 6
         self.saltando=False
-    
+        self.sensor_pies = pilas.fisica.Rectangulo(self.x, self.y, 5, 5, sensor=True, dinamica=False)
+
     def actualizar(self):
         self.x = self.figura.x
         self.y = self.figura.y
@@ -25,28 +26,24 @@ class Kuro(pilasengine.actores.Actor):
         if pilas.control.derecha:
             self.figura.x += 5
             self.espejado = False
-        if pilas.control.arriba:
+        if self.esta_pisando_el_suelo():
+            if self.pilas.control.arriba:
+                self.figura.impulsar(0, 5)
+
+        self.sensor_pies.x = self.x
+        self.sensor_pies.y = self.y - 20
+
+    def esta_pisando_el_suelo(self):
+        return len(self.sensor_pies.figuras_en_contacto) > 0
+"""        if pilas.control.arriba:
             if not self.saltando:
                 self.figura.y+=5
                 #self.hacer("Subir")
+"""
 
 pilas.actores.vincular(Kuro)
 
 kuro=Kuro(pilas)
-
-caja=pilas.actores.Caja()
-caja.aprender(pilas.habilidades.Arrastrable)
-
-pilas.fisica.definir_gravedad(0,0)
-
-fondo= pilas.fondos.Fondo("plataforma.png")
-
-rectangulo=pilas.fisica.Rectangulo(-170.0,18.0,355,30,dinamica=False)
-rectangulo=pilas.fisica.Rectangulo(264.0,15.8,100,30,dinamica=False)
-rectangulo=pilas.fisica.Rectangulo(181.3,-108.7,325,25,dinamica=False)
-rectangulo=pilas.fisica.Rectangulo(-4.0,137.2,155,25,dinamica=False)
-rectangulo=pilas.fisica.Rectangulo(231.8,190.8,180,25,dinamica=False)
-
-
+mapa=pilas.actores.MapaTiled("plataformas.tmx")
 
 pilas.ejecutar()
