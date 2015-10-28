6fa33f06729be9bd14a58365565237c8286805ed
brunoramosmarino <brunoramosmarino@gmail.com>
2015-10-28 14:29:30 -0300

    se agrego un mapa pero tiene un error esperando respuesta del foro de pilas

--- run.py
+++ run.py
@@ -4,12 +4,12 @@ import pilasengine
 import random
 
 pilas = pilasengine.iniciar()
-
+mapa=pilas.actores.MapaTiled("pacman3.tmx")
 class Kuro(pilasengine.actores.Actor):
     def iniciar(self):
         self.imagen = "mario.png"
 
 
-kuro=Kuro(pilas)
+#kuro=Kuro(pilas)
 
 pilas.ejecutar()
