ea09eb94179f7ee11809cc93a9c2f67b869e51ee
abelardogarcia <abeperrig_garcia@hotmail.com>
2015-10-28 11:25:23 -0300

    Agrego el menu(faltan funciones)

---
+++ menu.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+import pilasengine
+import sys
+
+pilas = pilasengine.iniciar()
+pilas.fondos.Selva()
+
+def niveles():
+    print "NIVELES"
+
+def iniciar_juego():
+    pilas.fondos.Espacio()
+    opciones = [
+        ('Comenzar a jugar', self.comenzar),
+        ('Ayuda',self.ayuda),
+        ('Niveles',niveles),
+        ('Salir', self.salir)]
+    self.menu = pilas.actores.Menu(opciones)
+
+def salir():
+    sys.exit(0)
+
+def ayuda():
+    print "Esctibir como va a funcionar el juego"
+
+menu = pilas.actores.Menu(
+            [
+                ('iniciar juego', iniciar_juego),
+                ('ayuda', ayuda),
+                ('niveles', niveles),
+                ('salir', salir),
+            ])
+
+pilas.ejecutar()
