b5db5508c5c0951cd7311e3903eb9ea35684f992
martinmontane <martin_montane_6@hotmail.com>
2015-11-03 22:22:38 -0300

    agregue el sensor de cambio de gravedad

--- run.py
+++ run.py
@@ -2,51 +2,52 @@
 # -*- coding: utf-8 -*-
 import pilasengine
 import random
-from compo import Subir
 
 pilas = pilasengine.iniciar()
-
-pilas.comportamientos.vincular(Subir)
 
 class Kuro(pilasengine.actores.Actor):
     def iniciar(self):
         self.imagen = "data/Kuro/mario.png"
         self.figura = pilas.fisica.Rectangulo(self.x, self.y, 20, 33)
         self.figura.sin_rotacion = True
-        self.figura.escala_de_gravedad = 6
-        self.saltando=False
-        self.sensor_pies = pilas.fisica.Rectangulo(self.x, self.y, 5, 5, sensor=True, dinamica=False)
+        self.figura.escala_de_gravedad = 2
+        self.sensor_pies = pilas.fisica.Rectangulo(self.x, self.y, 17, 5, sensor=True, dinamica=False)
 
     def actualizar(self):
+        velocidad = 10
+        salto = 5
         self.x = self.figura.x
         self.y = self.figura.y
+        
         if pilas.control.izquierda:
-            self.figura.x -= 5
+            self.figura.x -= velocidad
             self.espejado = True
+
         if pilas.control.derecha:
-            self.figura.x += 5
+            self.figura.x += velocidad
             self.espejado = False
+
         if self.esta_pisando_el_suelo():
             if self.pilas.control.arriba:
-                self.figura.impulsar(0, 5)
+                self.figura.impulsar(0, salto)
 
         self.sensor_pies.x = self.x
         self.sensor_pies.y = self.y - 20
 
     def esta_pisando_el_suelo(self):
         return len(self.sensor_pies.figuras_en_contacto) > 0
-"""        if pilas.control.arriba:
-            if not self.saltando:
-                self.figura.y+=5
-                #self.hacer("Subir")
-"""
+
+
+def si_pasa_por_el_medio(hola):
+    while True:
+        print hola
+
+medio = pilas.fisica.Rectangulo(0, 0, 636, 1, sensor=True, dinamica=True)
 
 pilas.actores.vincular(Kuro)
-
-kuro=Kuro(pilas)
+kuro=pilas.actores.Kuro()
 
 mapa=pilas.actores.MapaTiled("plataformas.tmx")
-
 
 pilas.ejecutar()
 
